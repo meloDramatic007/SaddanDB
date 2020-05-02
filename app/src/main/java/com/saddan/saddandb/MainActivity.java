7ea5f30d57f3dd7d13e5a9ff633bdb6f81b5b318
meloDramatic007 <saddanfah@gmail.com>
2020-05-02 15:33:17 +0600

    First release

---
+++ app/src/main/java/com/saddan/saddandb/MainActivity.java
@@ -0,0 +1,42 @@
+package com.saddan.saddandb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+{
+    private  String TAG =getClass().getSimpleName() ;
+    private SaddanDB saddanDB;
+    List<Movie> movieList=new ArrayList<>();
+    List<Movie> list_SaddanDB=new ArrayList<>();
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        saddanDB=new SaddanDB(this);
+
+        movieList.add(new Movie("hulk","1995"));
+        movieList.add(new Movie("Spider women","1995"));
+
+        saddanDB.setList("movie",movieList);
+
+
+
+        list_SaddanDB= (List<Movie>) saddanDB.getList("movie",Movie.class);
+
+        for(Movie movie:list_SaddanDB)
+        {
+            Log.d(TAG, "onCreate: "+movie.getName());
+        }
+
+    }
+}
---
+++ saddandb/src/main/java/com/saddan/saddandb/SaddanDB.java
@@ -0,0 +1,91 @@
+package com.saddan.saddandb;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SaddanDB
+{
+    private SharedPreferences preferences;
+
+    SaddanDB(Context appContext)
+    {
+        this.preferences = PreferenceManager.getDefaultSharedPreferences(appContext);
+    }
+
+    private void putListObject(String key, ArrayList<Object> objArray)
+    {
+        checkForNullKey(key);
+        Gson gson = new Gson();
+        ArrayList<String> objStrings = new ArrayList<String>();
+        for(Object obj : objArray){
+            objStrings.add(gson.toJson(obj));
+        }
+        putListString(key, objStrings);
+    }
+
+    private ArrayList<Object> getListObject(String key, Class<?> mClass){
+        Gson gson = new Gson();
+
+        ArrayList<String> objStrings = getListString(key);
+        ArrayList<Object> objects =  new ArrayList<Object>();
+
+        for(String jObjString : objStrings){
+            Object value  = gson.fromJson(jObjString,  mClass);
+            objects.add(value);
+        }
+        return objects;
+    }
+
+    private void checkForNullKey(String key)
+    {
+        if (key == null){
+            throw new NullPointerException();
+        }
+    }
+
+
+    private void putListString(String key, ArrayList<String> stringList)
+    {
+        checkForNullKey(key);
+        String[] myStringList = stringList.toArray(new String[stringList.size()]);
+        preferences.edit().putString(key, TextUtils.join("‚‗‚", myStringList)).apply();
+    }
+
+    private ArrayList<String> getListString(String key)
+    {
+        return new ArrayList<String>(Arrays.asList(TextUtils.split(preferences.getString(key, ""), "‚‗‚")));
+    }
+
+    void setList(String key, List<? extends Object> trendingListResponses)
+    {
+        ArrayList<Object> trendingObject = new ArrayList<Object>();
+        for(Object a : trendingListResponses){
+            trendingObject.add((Object) a);
+        }
+
+
+        putListObject(key, trendingObject);
+    }
+
+    @SuppressWarnings("unchecked")
+    <T> List<? extends Object> getList(String key,Class<T> Typex)
+    {
+        ArrayList<Object> trendingObject =getListObject(key, Typex);
+        ArrayList<T> trendingListResponses = new ArrayList<>();
+
+        for(Object objs : trendingObject)
+        {
+            trendingListResponses.add((T) objs);
+        }
+
+        return trendingListResponses;
+    }
+}
